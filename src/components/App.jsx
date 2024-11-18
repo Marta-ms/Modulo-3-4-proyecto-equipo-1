b463cb8bd92b5f0c1aa49eec992da0f47ead08b8
kendaltw <kendaltellez@gmail.com>
2024-11-18 11:39:37 +0100

    components

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,19 +1,14 @@
-import Logo from "../images/adalab.png";
-import Laptop from "../images/laptop-code-solid.svg";
 import "../scss/App.scss";
+import Header from "./Header";
+import Footer from "./Footer";
+
 
 function App() {
   return (
     <>
 <div className="container">
 
-<header className="header">
-  <a className="header__brand" href="./" title="Haz click para volver a la página inicial">
-    <img className="header__companyLogo" src={Laptop} alt="Logo proyectos molones"/>
-    <h1 className="header__title">Proyectos molones</h1>
-  </a>
-  <img className="logoSponsor" src={Logo} alt="Logo Adalab"/>
-</header>
+      <Header />
 
 <main className="main">
   <section className="hero">
@@ -84,10 +79,8 @@ function App() {
     
   </form>
 </main>
+    <Footer />
 
-<footer className="footer">
-  <img className="logoSponsor" src={Logo} alt="Logo Adalab"/>
-</footer>
 </div>
     </>
 
